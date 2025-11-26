bcf708dd8c2cc4f7e5c390b59443ba50dfa3e084
Maqed <magedibra@yahoo.com>
2025-11-26 17:22:31 +0200

    feat: add icon to each language

--- src/lib/learn.ts
+++ src/lib/learn.ts
@@ -1,11 +1,16 @@
+import NodejsIcon from "@/components/icons/nodejs";
+import PythonIcon from "@/components/icons/python";
+
 export const LANGUAGES = [
   {
     id: "python",
     name: "Python",
+    icon: PythonIcon,
   },
   {
     id: "nodejs",
     name: "Nodejs",
+    icon: NodejsIcon,
   },
 ];
 
