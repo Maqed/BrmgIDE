301b41f9bfce045a6c5fa9caf5eae663dbb907bb
Maqed <magedibra@yahoo.com>
2025-11-26 20:40:16 +0200

    chore: add language type

--- src/lib/learn.ts
+++ src/lib/learn.ts
@@ -1,7 +1,14 @@
 import NodejsIcon from "@/components/icons/nodejs";
 import PythonIcon from "@/components/icons/python";
+import React from "react";
 
-export const LANGUAGES = [
+export type LanguageType = {
+  id: string;
+  name: string;
+  icon: React.ComponentType<React.ComponentProps<"svg">>;
+};
+
+export const LANGUAGES: LanguageType[] = [
   {
     id: "python",
     name: "Python",
