2553459858fd6bcceb83be7d517f4d534e0b4493
Maqed <magedibra@yahoo.com>
2025-11-27 17:26:53 +0200

    feat: set Editor theme according to user's theme

--- src/components/ui/editor.tsx
+++ src/components/ui/editor.tsx
@@ -1,2 +1,16 @@
 "use client";
+import { Editor as MonacoEditor, EditorProps } from "@monaco-editor/react";
+import { useTheme } from "next-themes";
+
+export function Editor({ ...props }: EditorProps) {
+  const { theme } = useTheme();
+
+  return (
+    <MonacoEditor
+      theme={theme === "dark" ? "vs-dark" : "vs-light"}
+      {...props}
+    />
+  );
+}
+
 export * from "@monaco-editor/react";
