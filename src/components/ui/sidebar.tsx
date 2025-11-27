99d140e235adaa5a6456d7db0704a7896a915784
Maqed <magedibra@yahoo.com>
2025-11-27 17:47:02 +0200

    feat: add styles if it's open and not mobile

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -289,12 +289,19 @@ function SidebarRail({ className, ...props }: React.ComponentProps<"button">) {
   );
 }
 
-function SidebarInset({ className, ...props }: React.ComponentProps<"main">) {
+function SidebarInset({
+  openAndNotMobileClassName,
+  className,
+  ...props
+}: React.ComponentProps<"main"> & { openAndNotMobileClassName?: string }) {
+  const { open } = useSidebar();
+  const isMobile = useIsMobile();
   return (
     <main
       data-slot="sidebar-inset"
       className={cn(
         "bg-background relative flex w-full flex-1 flex-col",
+        open && !isMobile ? openAndNotMobileClassName : "",
         "md:peer-data-[variant=inset]:m-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow-sm md:peer-data-[variant=inset]:peer-data-[state=collapsed]:ml-2",
         className
       )}
