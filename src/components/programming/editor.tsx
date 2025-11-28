4fba8fbe67297b080ea99fe6eb16595dacfa057f
Maqed <magedibra@yahoo.com>
2025-11-28 18:16:11 +0200

    feat: add shared code context

--- src/components/programming/editor.tsx
+++ src/components/programming/editor.tsx
@@ -1,13 +1,19 @@
 "use client";
 import { Editor as MonacoEditor, EditorProps } from "@monaco-editor/react";
 import { useTheme } from "next-themes";
+import { useCode } from "./code-context";
 
-export function Editor({ ...props }: EditorProps) {
+export function Editor({ defaultValue, ...props }: EditorProps) {
   const { theme } = useTheme();
+  const { code, setCode } = useCode();
 
   return (
     <MonacoEditor
       theme={theme === "dark" ? "vs-dark" : "vs-light"}
+      value={code ?? defaultValue}
+      onChange={(value) => {
+        setCode(value ?? "");
+      }}
       {...props}
     />
   );
