08246ead8f59cb5d5275cb9cfadb429b88b8298d
Maqed <magedibra@yahoo.com>
2025-11-30 23:28:54 +0200

    fix: footer styles

--- src/components/layout/learn-footer.tsx
+++ src/components/layout/learn-footer.tsx
@@ -1,6 +1,8 @@
 import { getArticleAndItsNeighbours, SupportedLanguages } from "@/lib/learn";
-import { Card } from "fumadocs-ui/components/card";
-import { useLocale, useTranslations } from "next-intl";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Link } from "@/i18n/navigation";
+import { useTranslations } from "next-intl";
+import { BackwardChevron, ForwardChevron } from "../ui/chevrons";
 
 function LearnFooter({
   languageId,
@@ -13,7 +15,6 @@ function LearnFooter({
     languageId,
     articleLink,
   });
-  const locale = useLocale();
   const tLanguage = useTranslations(`languages.${languageId}.content`);
   if (!prev && !next) return null;
 
@@ -21,18 +22,42 @@ function LearnFooter({
     <div className="grid grid-cols-2">
       <div>
         {prev && (
-          <Card
-            title={tLanguage(prev.translationKey)}
-            href={`/${locale}/learn/${languageId}${prev.link}`}
-          />
+          <Link
+            className="no-underline"
+            href={`/learn/${languageId}${prev.link}`}
+          >
+            <Card
+              className="rounded-md h-16"
+              title={tLanguage(prev.translationKey)}
+            >
+              <CardHeader>
+                <CardTitle className="flex justify-start font-normal text-foreground/95 no-underline">
+                  <BackwardChevron className="size-5 text-foreground/80" />
+                  {tLanguage(prev.translationKey)}
+                </CardTitle>
+              </CardHeader>
+            </Card>
+          </Link>
         )}
       </div>
       <div>
         {next && (
-          <Card
-            title={tLanguage(next.translationKey)}
-            href={`/${locale}/learn/${languageId}${next.link}`}
-          />
+          <Link
+            className="no-underline"
+            href={`/learn/${languageId}${next.link}`}
+          >
+            <Card
+              className="rounded-md h-16"
+              title={tLanguage(next.translationKey)}
+            >
+              <CardHeader>
+                <CardTitle className="flex justify-end font-normal text-foreground/90 no-underline">
+                  {tLanguage(next.translationKey)}
+                  <ForwardChevron className="size-5 text-foreground/80" />
+                </CardTitle>
+              </CardHeader>
+            </Card>
+          </Link>
         )}
       </div>
     </div>
